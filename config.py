bf4484a39450238bb99a102c5797c57dc2564bf7
Arturo Ortiz Aguilar <54450112+SrArtur@users.noreply.github.com>
2023-07-22 15:13:54 +0200

    Configuración de logs para test y para la ejecuciÃon

--- config.py
+++ config.py
@@ -1,2 +1,17 @@
+import logging
+
+# Logging configuration
+logFormatter = logging.Formatter('%(name)s - [%(levelname)s] - %(asctime)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+logging.basicConfig(level=logging.INFO, format='%(name)s - [%(levelname)s] - %(asctime)s - %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
+rootLogger = logging.getLogger("TFM")
+rootLogger.setLevel(logging.WARNING)
+
+fileHandler = logging.FileHandler('logs/main.log')  # For logging to file
+fileHandler.setLevel(logging.INFO)
+fileHandler.setFormatter(logFormatter)
+rootLogger.addHandler(fileHandler)
+
+# Paths configuration
 adult_data_path = "data/adult.data"
 data_path = "data/"
