5f43a7ed80235397b557be80004c73c632343858
Arturo Ortiz Aguilar <54450112+SrArtur@users.noreply.github.com>
2023-08-23 17:45:53 +0200

    Cambio en el formato de la fecha

--- src/experiment_utils.py
+++ src/experiment_utils.py
@@ -14,9 +14,10 @@ def save_experiment_results(experiment_name, experiment_metrics):
     :return: None
     """
     current_time = datetime.now()
-    file_name = experiment_name + "_" + current_time.strftime("%Y-%m-%d_%H-%M-%S") + ".json"
+    file_name = experiment_name + "_" + current_time.strftime("%H-%M_%d-%m-%Y") + ".json"
     file_path = experiment_results_path + file_name
     with open(file_path, "w") as json_file:
         json.dump(experiment_metrics, json_file, indent=4)
 
     logger.info(f"Metrics saved as {file_name} in {experiment_results_path[:-1]}")
+    return file_path
